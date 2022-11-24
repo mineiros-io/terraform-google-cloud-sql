b216be2105a2cedd2fa0c38803be9303af6f080f
Soren Martius <soeren.martius@gmail.com>
2022-11-24 15:53:43 -0600

    chore: upgrade from template

--- test/unit-minimal/_generated_google.tf
+++ test/unit-minimal/_generated_google.tf
@@ -2,7 +2,8 @@
 // TERRAMATE: originated from generate_hcl block on /test/terramate_google.tm.hcl
 
 variable "gcp_project" {
-  description = "(Required) The ID of the project in which the resource belongs."
+  default     = null
+  description = "(Optional) The ID of the project in which the resource belongs."
   type        = string
 }
 variable "gcp_region" {
@@ -16,7 +17,7 @@ variable "gcp_org_domain" {
   type        = string
 }
 variable "gcp_billing_account" {
-  default     = null
+  default     = "test-billing-account"
   description = "(Optional) The billing account to use when creating projects."
   type        = string
 }
@@ -24,6 +25,10 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
+      version = "4.4"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
       version = "4.4"
     }
     random = {
@@ -36,6 +41,10 @@ provider "google" {
   project = var.gcp_project
   region  = var.gcp_region
 }
+provider "google-beta" {
+  project = var.gcp_project
+  region  = var.gcp_region
+}
 provider "random" {
 }
 resource "random_id" "suffix" {
@@ -43,17 +52,20 @@ resource "random_id" "suffix" {
   count       = local.test_name != "unit-disabled" ? 1 : 0
 }
 data "google_project" "project" {
+  count      = var.gcp_project != null ? 1 : 0
+  project_id = var.gcp_project
 }
 data "google_organization" "org" {
-  count  = local.org_domain != null ? 1 : 0
-  domain = local.org_domain
+  count  = var.gcp_org_domain != null ? 1 : 0
+  domain = var.gcp_org_domain
 }
 locals {
-  billing_account = var.gcp_billing_account
-  org_domain      = var.gcp_org_domain
-  org_id          = try(data.google_organization.org[0].name, null)
-  project_id      = var.gcp_project
-  project_number  = data.google_project.project.number
-  random_suffix   = try(random_id.suffix[0].hex, "disabled")
-  test_name       = "unit-minimal"
+  billing_account       = var.gcp_billing_account
+  directory_customer_id = try(data.google_organization.org[0].directory_customer_id, "no-directoy-customer")
+  org_domain            = try(data.google_organization.org[0].domain, "no-domain.mineiros.io")
+  org_id                = try(data.google_organization.org[0].org_id, null)
+  project_id            = try(data.google_project.project[0].project_id, "no-project")
+  project_number        = try(data.google_project.project[0].number, null)
+  random_suffix         = try(random_id.suffix[0].hex, "disabled")
+  test_name             = "unit-minimal"
 }
